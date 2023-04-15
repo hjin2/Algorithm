dc57205a5d55a46a93f96ae9cd8834e4e4a30e60
효진 <72199096+hjin2@users.noreply.github.com>
2023-04-16 01:14:51 +0900

    Update 연산자 끼워넣기.java

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,63 +1,90 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	public static int MAX = Integer.MIN_VALUE;	// 최댓값 
-	public static int MIN = Integer.MAX_VALUE;	// 최솟값 
-	public static int[] operator = new int[4];	// 연산자 개수 
-	public static int[] number;					// 숫자 
-	public static int N;						// 숫자 개수 
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		N = in.nextInt();
-		number = new int[N];
- 
-		// 숫자 입력
-		for (int i = 0; i < N; i++) {
-			number[i] = in.nextInt();
+	static int resultMax = Integer.MIN_VALUE;
+	static int resultMin = Integer.MAX_VALUE;
+	static int n;
+	static int[] nums; 
+	static String[] selected;
+	static String[] operator = { "+", "-", "x", "/" };
+	static boolean[] visited;
+	static List<String> list;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		nums = new int[n];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
- 
-		// 연산자 입력
+
+		st = new StringTokenizer(br.readLine());
+		int[] tmps = new int[4];
 		for (int i = 0; i < 4; i++) {
-			operator[i] = in.nextInt();
+			tmps[i] = Integer.parseInt(st.nextToken());
 		}
- 
-		dfs(number[0], 1);
- 
-		System.out.println(MAX);
-		System.out.println(MIN);
- 
-	}
- 
-	public static void dfs(int num, int idx) {
-		if (idx == N) {
-			MAX = Math.max(MAX, num);
-			MIN = Math.min(MIN, num);
-			return;
-		}
- 
+		
+		list = new ArrayList<>();
 		for (int i = 0; i < 4; i++) {
-			// 연산자 개수가 1개 이상인 경우
-			if (operator[i] > 0) {
- 
-				// 해당 연산자를 1 감소시킨다.
-				operator[i]--;
- 
-				switch (i) {
- 
-				case 0:	dfs(num + number[idx], idx + 1);	break;
-				case 1:	dfs(num - number[idx], idx + 1);	break;
-				case 2:	dfs(num * number[idx], idx + 1);	break;
-				case 3:	dfs(num / number[idx], idx + 1);	break;
- 
-				}
-				// 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
-				operator[i]++;
+			for (int j = 0; j < tmps[i]; j++) {
+				list.add(operator[i]);
 			}
 		}
+
+		
+		// 5P5
+		// { 0, 1, 2, 3 }
+		// { - x x + + + }
+		selected = new String[n - 1];
+		visited = new boolean[n - 1];
+		p(0);
+		System.out.println(resultMax);
+		System.out.println(resultMin);
+
 	}
- 
+
+	private static void p(int cnt) {
+		if (cnt == n-1) {
+			calc();
+			return;
+		}
+		for (int i = 0; i < n-1; i++) {
+			if (visited[i])	continue;
+			selected[cnt] = list.get(i);
+			visited[i] = true;
+			p(cnt+1);
+			visited[i] = false;
+		}
+	}
+
+	private static void calc() {
+		int value = nums[0];
+		for(int i = 0 ; i < n-1; i++) {
+			switch(selected[i]) {
+			case "+":
+				value = value + nums[i+1]; 
+				break;
+			case "-":
+				value = value - nums[i+1]; 
+				break;
+			case "x":
+				value = value * nums[i+1]; 
+				break;
+			case "/":
+				value = value / nums[i+1]; 
+				break;
+			
+			}
+		}
+		resultMax = resultMax < value ? value : resultMax;
+		resultMin = resultMin > value ? value : resultMin;
+		
+	}
 }
