686110e016864de8d6d93f7cdf43ee5f74678c01
효진 <72199096+hjin2@users.noreply.github.com>
2023-09-04 21:43:28 +0900

    [level 2] Title: 전화번호 목록, Time: 214.60 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,16 +1,21 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book); // 전화번호를 정렬
-
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                // 다음 번호가 현재 번호의 접두사인 경우
-                return false;
+        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i < phone_book.length ; i++){
+            map.put(phone_book[i],i);
+        }
+        
+        for(int i = 0 ; i < phone_book.length ; i++){
+            for(int j = 1 ; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0,j))){
+                    answer = false;
+                    return answer;
+                }
             }
         }
-
-        return true; // 모든 번호가 서로의 접두사가 아닌 경우
+        return answer;
     }
 }
