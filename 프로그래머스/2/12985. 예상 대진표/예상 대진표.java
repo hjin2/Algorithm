52fb1051ab6890043ed9165be9c61c973ada49ae
효진 <72199096+hjin2@users.noreply.github.com>
2023-12-30 00:02:32 +0900

    Update 예상 대진표.java

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,15 +2,35 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-	int answer = 0;
+        int answer = 0;
+        int cnt = 1;
+        while(true){
+            
+            if(a < b && a % 2 == 1 && a+1 == b && b-1 == a){
+                answer = cnt;
+                break;
+            }if( b < a && b % 2 == 1 && b+1 == a && a-1 == b){
+                 answer = cnt;
+                break;
+            }
+            
+              if(a%2==1){
+                    a = (a / 2) + 1;
+                }else{
+                    a = (a / 2);
+                }
 
-	while(b != a) {
-		a = ++a / 2;
-		b = ++b / 2;
-		answer++;
-	}
+                if(b%2 == 1){
+                    b = (b / 2) + 1;
+                }else{
+                    b = (b / 2);
+                }
 
-	return answer;
+                cnt++;
+                            
+        }
 
+        return answer;
     }
+    
 }
