0a23389b2c89e060f1e3e4a33c0188db7d6f1126
효진 <72199096+hjin2@users.noreply.github.com>
2024-01-02 11:55:28 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 108 ms, Memory: 12828 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -1,36 +1,37 @@
-import java.util.*;
-public class Main
-{
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int k = sc.nextInt();
-        int result = 0;
+        int all = n + m;
 
-
-        int girl = n / 2;
-        if(m >= girl){
-            result = girl;
-        }else{
-            result = m;
+        int team = 0;
+        while(n>=2 && m>=1){
+            n -= 2;
+            m -= 1;
+            team++;
         }
 
 
-        girl = n - 2*result;
-        int boy = m - result;
+        if(all - ( 3 * team) >= k){ // 팀을 최대로 결성하고 남은애들이 k보다 같거나 크면
+            System.out.println(team); // 그 팀은 다 나갈 수 있다
 
+        }else{ // 남은애들로 어학연수 못보내면 팀을 해체시켜야해
+            // 몇 팀을 해체시켜야 하는가?
 
-
-        if(girl+boy < k){
-            k = k - (girl+boy);
-            if(k%3==0){
-                result -= (k/3);
+            k = k - (all - ( 3 * team)); // 일단 남은애는 다 보내고
+//            System.out.println(" k : " + k);
+            // 그 다음에 남은 애를 보고 몇 팀을 해체할 것인가
+            if(k % 3 == 0){
+                team -= k / 3;
             }else{
-                result -= (k/3) + 1;
+                team = team - (k / 3 + 1);
             }
+            System.out.println(team);
         }
 
-        System.out.println(result);
     }
 }
