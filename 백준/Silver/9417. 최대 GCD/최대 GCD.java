2f86c9e068369d548e6097640e5f6e994b08c546
효진 <72199096+hjin2@users.noreply.github.com>
2024-01-08 13:44:44 +0900

    [Silver IV] Title: 최대 GCD, Time: 72 ms, Memory: 11316 KB -BaekjoonHub

--- 백준/Silver/9417. 최대 GCD/최대 GCD.java
+++ 백준/Silver/9417. 최대 GCD/최대 GCD.java
@@ -1,36 +1,41 @@
-import java.io.*;
-import java.util.*;
-public class Main
-{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = null;
+        int n = Integer.parseInt(br.readLine());
 
-
-        int tc = Integer.parseInt(br.readLine());
-        while(tc-- > 0){
-            int result = -1;
-            int[] arr = new int[100];
-            int idx = 0;
+        int[] nums = new int[100];
+        for(int testcase = 0 ; testcase < n ; testcase++){
             st = new StringTokenizer(br.readLine());
+            int result = Integer.MIN_VALUE;
+            int idx = 0;
             while(st.hasMoreTokens()){
-                arr[idx] = Integer.parseInt(st.nextToken());
-                idx++;
+                int a = Integer.parseInt(st.nextToken());
+                nums[idx++] = a;
             }
-            for(int x = 0 ; x < idx ; x++){
-                for(int y = 0 ; y < idx ; y++){
-                    if(x >= y){
-                        continue;
-                    }
-                    for(int z = 1 ; z <= Math.min(arr[x],arr[y]) ; z++){
-                        if( arr[x] % z == 0 && arr[y] % z == 0){
-                            result = result < z ? z : result;
-                        }
-                    }
+
+            for(int i = 0 ; i < idx ; i++){
+                for(int j = i+1 ; j < idx ; j++){
+                    int gcd = gcd(nums[i], nums[j]);
+                    result = result < gcd ? gcd : result;
                 }
             }
             System.out.println(result);
         }
+    }
 
+    public static int gcd(int a, int b){
+        while(a % b != 0){
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a > b ? b : a;
     }
 }
