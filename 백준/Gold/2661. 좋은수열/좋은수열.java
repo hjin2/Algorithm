e08f16b182fd6d8bf166868ef110190d2b19dcbd
효진 <72199096+hjin2@users.noreply.github.com>
2024-02-07 10:07:49 +0900

    [Gold IV] Title: 좋은수열, Time: 120 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.java
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-    static int start = 1;
-    static int end = 3;
-    static int n;
+    static int N;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-
+        N = sc.nextInt();
+        
         rec("");
     }
-
     public static void rec(String str){
-        if(str.length() == n){
+        if(str.length() == N){
             System.out.println(str);
             System.exit(0);
         }
-
+        
         for(int i = 1 ; i <= 3 ; i++){
-            if(can_make_str(str + i)){
+            if(goodnum(str + i)){
                 rec(str + i);
             }
         }
     }
-    public static boolean can_make_str(String str){
+    
+    public static boolean goodnum(String str){
         for(int i = 1 ; i <= str.length() / 2 ; i++){
+            String front = str.substring(str.length() - i * 2, str.length() - i);
             String back = str.substring(str.length() - i, str.length());
-            String front = str.substring(str.length() - i * 2 , str.length() - i);
-            if(front.equals(back)) return false;
+            if(front.equals(back)) {
+                return false;
+            }
         }
         return true;
     }
