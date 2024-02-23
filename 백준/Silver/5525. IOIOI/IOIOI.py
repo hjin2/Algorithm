8b03744715b25e8e42472d5473aff5eb0b262a89
효진 <72199096+hjin2@users.noreply.github.com>
2024-02-23 22:51:37 +0900

    [Silver I] Title: IOIOI, Time: 136 ms, Memory: 112140 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,14 +1,26 @@
 import sys
 
-input = sys.stdin.readline
-N = int(input())
-M = int(input())
-S = input().rstrip()
-p = 'I' + 'OI' * N
+si = sys.stdin.readline
 
-count = 0
-for start in [i for i in range(M - len(p) + 1) if S[i] == 'I']:
-    end = start + len(p)
-    if S[start:end] == p:
-        count += 1
-print(count)
+n = int(si())
+m = int(si())
+s = si()
+
+
+
+ans = 0
+cnt = 0
+i = 0
+while i < (m- 1):
+    if s[i:i+3]  == 'IOI': # 일단 될 수 있음
+        i += 2
+        cnt += 1 # 그 뒤에도 나올 수 있는지?
+        if cnt == n:
+            ans += 1
+            cnt -= 1 # 앞에꺼 사라지니까 cnt -1
+    else:
+        i += 1
+        cnt = 0 # 없어져서 리셋됨
+
+
+print(ans)
