8ba8ace67d11e78c5b4385d7ded13fc710091cf6
효진 <72199096+hjin2@users.noreply.github.com>
2024-03-01 22:22:21 +0900

    [Silver II] Title: 연속합, Time: 184 ms, Memory: 21276 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,41 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        // 수는 한개 이상
+        int start = 0;
+        int end = 0;
+        int result = Integer.MIN_VALUE;
+        int sum = 0;
+        while(end < N){ 
+            sum += arr[end];
+            if(result < sum){
+                result = sum;
+            }
+            if(sum < 0){
+                start = end + 1;
+                end = start;
+                if(start == N) break;
+                sum = 0;
+            }else{
+                end++;
+            }
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int nums[] = new int[n];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0;i<n;i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int l = 0;
-		int r = 0;
-		int sum = 0;
-		int max = -1001;
-		
-		while(r<n) {
-			sum += nums[r];
-			max = Math.max(max, sum);
-			
-			if(sum < 0) {
-				l = r+1;
-				r = l;
-				if(l == n) break;
-				sum = 0;
-			}else {
-				
-				r++;
-			}
-			
-			
-		}
-		max = Math.max(max, sum);
-		System.out.println(max);
-		
-	}
+
+
+        }
+        result = Math.max(result,sum);
+        System.out.println(result);
+    }
 
 }
