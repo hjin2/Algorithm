4ca2da628be59d457f986fdfa568bf19d1ee9aee
효진 <72199096+hjin2@users.noreply.github.com>
2024-04-11 10:55:24 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -5,32 +5,34 @@ import java.io.InputStreamReader;
 public class Main {
     public static int result = 0;
     public static int n;
-    public static int[] arr;
+    public static int[] dp;
+
     public static void main(String[] args) throws IOException {
         // 2 * n 크기의 직사각형을 1 * 2, 2 * 1 타일로 채우는 방법의 수
         // 행 * 열
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[1001];
-        // 2 * n(행)
-
-        arr[1] = 1; // 열의 길이가 1일 때 생기는 경우의 수 : 1
-        arr[2] = 2; // 열의 길이가 2일 때 생기는 경우의 수 : 2
+        n = Integer.parseInt(br.readLine());            // 2 * n(행)
+        dp = new int[1001]; // 크기 최대로 만들어놓기
 
 
-        for(int i = 3 ; i < n ; i++){
+        dp[1] = 1; // 열의 길이가 1일 때 생기는 경우의 수 : 1
+        dp[2] = 2; // 열의 길이가 2일 때 생기는 경우의 수 : 2
+
+
+        for (int i = 3; i <= n; i++) {
+//            dp[i] = (dp[i-2] + dp[i-1]) % 10007;
             rec(i);
         }
-        System.out.println(rec(n));
-//        System.out.println(result);
+        System.out.println(dp[n]);
     }
-    public static int rec(int col){ // 열의 길이가 col일 때 생기는 경우의 수
-        if(col == 1){
-            return arr[1];
-        }else if(col == 2){
-            return arr[2];
+
+    public static int rec(int col) { // 열의 길이가 col일 때 생기는 경우의 수
+        if (col == 1) {
+            return dp[1];
+        } else if (col == 2) {
+            return dp[2];
         }
-        return arr[col] = ((arr[col-1]) + (arr[col-2]))%10007;
+        return dp[col] = (dp[col-2] + dp[col-1]) % 10007;
     }
 }
