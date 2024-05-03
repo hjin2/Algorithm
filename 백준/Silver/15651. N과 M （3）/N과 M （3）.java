d785e14d9cbd3a6842d32d0ec65003ed3c69bccd
효진 <72199096+hjin2@users.noreply.github.com>
2024-05-03 10:52:32 +0900

    [Silver III] Title: N과 M (3), Time: 364 ms, Memory: 112012 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,31 +1,57 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n,m;
-    static int[] arr;
-    static StringBuilder sb;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static int N,M;
+    public static int[] selected;
+    public static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        n = sc.nextInt();
-        m = sc.nextInt();
-        arr = new int[m];
 
-        comb(0);
+        selected = new int[M];
+        // 중복순열
+        /*
+        11
+        12
+        13
+        14
+        21
+        22
+        23
+        24
+        31
+        32
+        33
+        34
+        41
+        42
+        43
+        44
+         */
+
+        p(0);
         System.out.println(sb);
     }
-    public static void comb(int depth){
-        if(depth == m){
-            for(int num : arr){
-                sb.append(num).append(" ");
+
+    public static void p(int m){
+        if(m == M){
+            for(int i = 0 ; i < m ; i++){
+                sb.append(selected[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for(int i = 1 ; i <= n ; i++){
-            arr[depth] = i;
-            comb(depth + 1);
+
+        for(int i = 1 ; i <= N ; i++){
+            selected[m] = i;
+            p(m + 1);
         }
     }
 }
