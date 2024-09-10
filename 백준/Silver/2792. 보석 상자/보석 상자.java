365c0b073794c7ed4d222f0bc9911fd3ddf2d2fa
효진 <72199096+hjin2@users.noreply.github.com>
2024-09-10 19:44:39 +0900

    [Silver I] Title: 보석 상자, Time: 276 ms, Memory: 34928 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.java
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -1,49 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int result = -1;
+    public static int n, m;
+    public static int[] nums;
+    public static int result = 0;
+    public static int max = -1;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] colorsCnt = new int[m];
-        for(int i = 0 ; i<m;i++){
-            colorsCnt[i] = Integer.parseInt(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        nums = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, nums[i]);
         }
 
-        Arrays.sort(colorsCnt);
-        int left = 1;
-        int right = colorsCnt[m-1];
-        
+        int s = 1;
+        int e = max;
+        int mid;
 
-        while(left <= right){
-            int mid = (left+right) / 2;
-            int sum = 0;
+        while (s <= e) {
+            mid = (s + e) / 2;
+            int person = n; // 매번 새롭게 초기화
 
-            int person = 0;
-            for(int i = 0 ; i < m ; i++){
-                if(colorsCnt[i] % mid ==0 ){
-                    person += colorsCnt[i] / mid;
-                }else{
-                    person += colorsCnt[i] / mid +1;
+            // 최대 mid개만큼 나누어줄 것이다.
+            for (int i = 0; i < m; i++) {
+                if (nums[i] <= mid) {
+                    person -= 1;
+                } else { // nums[i] > mid
+                    int tmp = nums[i] / mid;
+                    if (nums[i] % mid != 0) {
+                        tmp += 1;
+                    }
+                    person -= tmp;
                 }
             }
 
-            if(person >  n){
-               left = mid + 1;
-            }else{
-                right = mid -1;
-                result = mid;
+            if (person < 0) {
+                // 사람이 음수라는 것 -> mid개로 나누어주려니까 더 많은 사람이 필요하다
+                s = mid + 1;
+            } else {
+                // 사람이 충분하거나 남을 수 있다 -> mid보다 더 적게 나눠줄 수 있다
+                e = mid - 1;
+                result = mid; // 최적의 mid 값 저장
             }
         }
 
         System.out.println(result);
-
-
     }
 }
